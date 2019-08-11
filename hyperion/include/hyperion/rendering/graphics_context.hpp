95536af4f3a26efeb37d005c7efcc164689a3081
Thimilius <mail@thimilius.com>
2019-08-11 13:41:49 +0200

    Remove duplicate graphics context

--- hyperion/include/hyperion/rendering/graphics_context.hpp
+++ hyperion/include/hyperion/rendering/graphics_context.hpp
@@ -9,19 +9,6 @@ namespace Hyperion::Rendering {
         virtual void Init() = 0;
         virtual void SwapBuffers() = 0;
         virtual void SetVSyncMode(EVSyncMode vsync_mode) = 0;
-
-        template<typename... Args>
-        static CGraphicsContext *Do(Args... args) {
-            return nullptr;
-        }
-
-        template<typename... Args>
-        static CGraphicsContext *Create(Args... args) {
-            switch (CRenderAPI::GetAPI()) {
-                case ERenderAPI::OpenGL: return COpenGLGraphicsContext::Create();
-                default: HYP_ASSERT_ENUM_OUT_OF_RAGE; return nullptr;
-            }
-        }
     };
 
 }
