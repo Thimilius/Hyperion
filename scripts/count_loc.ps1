905fe3cd23117a631421ba4053e81641b181e14d
Thimilius <mail@thimilius.com>
2020-01-16 23:39:29 +0100

    Add powershell script to count lines of code

---
+++ scripts/count_loc.ps1
@@ -0,0 +1,14 @@
+$base_path = Resolve-Path ($PSScriptRoot + "/../projects/hyperion")
+
+$loc_include = (gci $base_path/include -include *.hpp,*.cpp -recurse | select-string .).Count
+$loc_src = (gci $base_path/src -include *.hpp,*.cpp -recurse | select-string .).Count
+$loc_all = $loc_include + $loc_src
+
+Write-Host "Lines of code: " -NoNewLine
+Write-Host $loc_all -ForegroundColor Yellow
+
+Write-Host "    - include: " -NoNewLine
+Write-Host $loc_include -ForegroundColor Yellow
+
+Write-Host "    - src:     " -NoNewLine
+Write-Host $loc_src -ForegroundColor Yellow
