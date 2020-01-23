c9f2705a512b2b290cf1064667ea6fbd84d1d2bb
Thimilius <mail@thimilius.com>
2020-01-23 22:30:20 +0100

    Add basic light struct

---
+++ projects/hyperion/include/hyperion/rendering/light.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "hyperion/core/color.hpp"
+#include "hyperion/core/math/vec3.hpp"
+
+namespace Hyperion::Rendering {
+
+    enum class LightType {
+        Directional,
+        Point,
+        Spot
+    };
+
+    struct Light {
+        LightType type;
+
+        Color color;
+
+        Vec3 position;
+        Vec3 direction;
+
+        // TODO: Revisit these properties as they might be to detailed
+        struct {
+            union {
+                f32 attenuation_constant;
+                f32 attenuation_linear;
+                f32 attenuation_quadratic;
+            };
+            union {
+                f32 spot_inner_radius;
+                f32 spot_outer_radius;
+            };
+        };
+    };
+
+}
