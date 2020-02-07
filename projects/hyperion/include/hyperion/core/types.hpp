bcdd72a2bf9ebd89f24d6ccadff7299c0e40b99f
Thimilius <mail@thimilius.com>
2020-02-07 20:12:32 +0100

    Move type usings out of hyperion namespace

--- projects/hyperion/include/hyperion/core/types.hpp
+++ projects/hyperion/include/hyperion/core/types.hpp
@@ -2,34 +2,30 @@
 
 #include <cstdint>
 
-namespace Hyperion {
+using s8 = int8_t;
+using s16 = int16_t;
+using s32 = int32_t;
+using s64 = int64_t;
 
-    using s8 = int8_t;
-    using s16 = int16_t;
-    using s32 = int32_t;
-    using s64 = int64_t;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
 
-    using u8 = uint8_t;
-    using u16 = uint16_t;
-    using u32 = uint32_t;
-    using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
 
-    using f32 = float;
-    using f64 = double;
+template<typename T>
+using Ref = std::shared_ptr<T>;
 
-    template<typename T>
-    using Ref = std::shared_ptr<T>;
+template<typename T>
+using Scope = std::unique_ptr<T>;
 
-    template<typename T>
-    using Scope = std::unique_ptr<T>;
+template<typename T>
+using Vector = std::vector<T>;
 
-    template<typename T>
-    using Vector = std::vector<T>;
+template<typename Key, typename Value>
+using Map = std::unordered_map<Key, Value>;
 
-    template<typename Key, typename Value>
-    using Map = std::unordered_map<Key, Value>;
-
-    template<typename T>
-    using Set = std::set<T>;
-
-}
+template<typename T>
+using Set = std::set<T>;
