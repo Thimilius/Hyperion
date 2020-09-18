7571777338b66e9018d7205f86a7092a3173a212
Thimilius <mail@thimilius.com>
2020-09-18 18:51:12 +0200

    Test mono internal method registration and method calls

--- projects/hyperion/src/scripting/scripting_engine.cpp
+++ projects/hyperion/src/scripting/scripting_engine.cpp
@@ -7,6 +7,13 @@
 
 namespace Hyperion {
 
+    void Log(MonoString *message) {
+        MonoError error;
+        char* utf8_message = mono_string_to_utf8_checked(message, &error);
+        HYP_TRACE(utf8_message);
+        mono_free(utf8_message);
+    }
+
     void ScriptingEngine::Init() {
         mono_set_dirs("data/mono/lib", "data/mono/etc");
 
@@ -17,6 +24,34 @@ namespace Hyperion {
         } else {
             HYP_LOG_INFO("Scripting", "Initialized Mono scripting engine!");
         }
+
+        mono_add_internal_call("HyperionEngine.Application::Log", Log);
+
+        char *assemblyPath = "data/managed/HyperionEngine.dll";
+        MonoAssembly *assembly = mono_domain_assembly_open(s_root_domain, assemblyPath);
+        if (!assembly) {
+
+        }
+
+        MonoImage *image = mono_assembly_get_image(assembly);
+        if (!image) {
+
+        }
+
+        MonoMethodDesc *TypeMethodDesc;
+        char *TypeMethodDescStr = "Application::Do()";
+        TypeMethodDesc = mono_method_desc_new(TypeMethodDescStr, false);
+        if (!TypeMethodDesc) {
+
+        }
+
+        //Search the method in the image
+        MonoMethod *method = mono_method_desc_search_in_image(TypeMethodDesc, image);
+        if (!method) {
+
+        }
+
+        mono_runtime_invoke(method, nullptr, nullptr, nullptr);
     }
 
     void ScriptingEngine::Shutdown() {
--- projects/hyperion_engine/Application.cs
+++ projects/hyperion_engine/Application.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace HyperionEngine {
     public static class Application {
         public static void Do() {
             Console.WriteLine("Hello from managed!");
+
+            Log("String passed from managed");
         }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern static void Log(string message);
     }
 }
