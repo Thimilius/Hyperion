5be36a60dff18a470f6c7ebb4dafc01d36fc5289
Thimilius <mail@thimilius.com>
2021-01-03 01:05:24 +0100

    Prepare for rewrite

--- projects/tools/hyperion-editor/src/editor/editor_application.cpp
+++ projects/tools/hyperion-editor/src/editor/editor_application.cpp
@@ -1,10 +1,6 @@
 ﻿#include <hyperion/hyperion.hpp>
 #include <hyperion/entry_point.hpp>
 
-#include "hyperion/editor/editor_engine.hpp"
-#include "hyperion/editor/editor_selection.hpp"
-#include "hyperion/editor/editor_render_pipeline.hpp"
-
 namespace Hyperion::Editor {
 
     class EditorApplication : public Application {
@@ -12,19 +8,19 @@ namespace Hyperion::Editor {
         EditorApplication(const ApplicationSettings &settings) : Application(settings) { }
     protected:
         void OnInit() override {
-            EditorEngine::OnInit();
+
         }
 
         void OnUpdate(f32 delta_time) override {
-            EditorEngine::OnUpdate(delta_time);
+
         }
 
         void OnTick() override {
-            EditorEngine::OnTick();
+
         }
 
         void OnEvent(Event &event) override {
-            EditorEngine::OnEvent(event);
+
         }
     };
 
@@ -32,8 +28,5 @@ namespace Hyperion::Editor {
 
 Hyperion::Application *Hyperion::CreateApplication() {
     ApplicationSettings settings = ApplicationSettings::FromJsonFile("app.json");
-    settings.render.path = Rendering::RenderPath::Custom;
-    settings.render.custom_pipeline = new Editor::EditorRenderPipeline();
-
     return new Hyperion::Editor::EditorApplication(settings);
 }
