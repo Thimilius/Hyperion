4f98cf0f430fafe851af007097dc8fd6884c22c0
Thimilius <mail@thimilius.com>
2021-02-02 21:16:55 +0100

    Fix locking in auto reset event

--- projects/hyperion/source/core/threading/auto_reset_event.cpp
+++ projects/hyperion/source/core/threading/auto_reset_event.cpp
@@ -2,10 +2,6 @@
 
 #include "hyperion/core/threading/auto_reset_event.hpp"
 
-// NOTE: Usually you would use a lock in the Notify and WaitUnblocked functions as well.
-// But because in our scenario they are only ever called on one thread that is not really necessary.
-// At least I think so...
-
 namespace Hyperion::Threading {
 
     AutoResetEvent::AutoResetEvent(bool start_value) {
@@ -13,6 +9,8 @@ namespace Hyperion::Threading {
     }
 
     void AutoResetEvent::Notify() {
+        std::unique_lock<std::mutex> lock(m_mutex);
+
         m_signaled = true;
         m_condition_variable.notify_one();
     }
