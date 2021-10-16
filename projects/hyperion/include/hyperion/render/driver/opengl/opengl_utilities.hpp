72f4ab8a89a10454f4c81faa4363c09e453e723f
Thimilius <mail@thimilius.com>
2021-10-16 11:05:13 +0200

    Add opengl utilities

---
+++ projects/hyperion/include/hyperion/render/driver/opengl/opengl_utilities.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+//---------------------- Library Includes ----------------------
+#include <glad/glad.h>
+
+//---------------------- Project Includes ----------------------
+#include "hyperion/render/types/render_types_general.hpp"
+#include "hyperion/render/types/render_types_mesh.hpp"
+#include "hyperion/render/types/render_types_texture.hpp"
+
+//-------------------- Definition Namespace --------------------
+namespace Hyperion::Rendering {
+
+    class OpenGLUtilities final {
+    public:
+        static GLbitfield GetClearFlags(ClearFlags clear_flags);
+
+        static uint32 GetBytesPerPixelForTextureFormat(TextureFormat format);
+        static void SetUnpackAlignmentForTextureFormat(TextureFormat format);
+        static void FlipTextureHorizontally(uint32 width, uint32 height, TextureFormat format, Array<byte> &pixels);
+        static GLenum GetTextureFormat(TextureFormat format);
+        static GLenum GetTextureFormatType(TextureFormat format_type);
+        static GLenum GetTextureInternalFormat(TextureFormat internal_format);
+        static GLint GetTextureWrapMode(TextureWrapMode wrap_mode);
+        static GLint GetTextureMinFilter(TextureFilter filter);
+        static GLint GetTextureMaxFilter(TextureFilter filter);
+        static GLfloat GetTextureAnisotropicFilter(TextureAnisotropicFilter anisotropic_filter);
+
+        static GLenum GetRenderTextureInternalFormat(RenderTextureFormat internal_format);
+
+        static GLenum GetTopology(MeshTopology mesh_topology);
+        static GLuint GetAttributeIndexForVertextAttributeSize(VertexAttributeKind kind);
+        static GLenum GetVertexAttributeType(VertexAttributeType vertex_attribute_type);
+        static GLuint GetVertexAttributeSizeForVertexAttribute(VertexAttributeType vertex_attribute_type, uint32 dimension);
+    private:
+        OpenGLUtilities() = delete;
+        ~OpenGLUtilities() = delete;
+    };
+
+}
--- projects/hyperion/source/render/driver/opengl/opengl_render_driver.cpp
+++ projects/hyperion/source/render/driver/opengl/opengl_render_driver.cpp
@@ -1,196 +1,16 @@
 //----------------- Precompiled Header Include -----------------
 #include "hyppch.hpp"
 
-#include <array>
-
 //--------------------- Definition Include ---------------------
 #include "hyperion/render/driver/opengl/opengl_render_driver.hpp"
 
 //---------------------- Project Includes ----------------------
 #include "hyperion/core/app/display.hpp"
 #include "hyperion/render/driver/opengl/opengl_render_driver_shader_compiler.hpp"
+#include "hyperion/render/driver/opengl/opengl_utilities.hpp"
 
 //-------------------- Definition Namespace --------------------
 namespace Hyperion::Rendering {
-
-    //--------------------------------------------------------------
-    uint32 GetBytesPerPixelForTextureFormat(TextureFormat format) {
-        switch (format) {
-            case TextureFormat::RGBA32: return 4;
-            case TextureFormat::RGB24: return 3;
-            case TextureFormat::R8: return 1;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    void SetUnpackAlignmentForTextureFormat(TextureFormat format) {
-        GLint alignment = 4;
-        switch (format) {
-            case TextureFormat::RGB24: alignment = 4; break;
-            case TextureFormat::RGBA32: alignment = 4; break;
-            case TextureFormat::R8: alignment = 1; break;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE;
-        }
-        glPixelStorei(GL_UNPACK_ALIGNMENT, alignment);
-    }
-
-    //--------------------------------------------------------------
-    void FlipTextureHorizontally(uint32 width, uint32 height, TextureFormat format, Array<byte> &pixels) {
-        uint32 stride = width * GetBytesPerPixelForTextureFormat(format);
-        Array<byte> temp_buffer(stride);
-        byte *temp_buffer_data = temp_buffer.GetData();
-
-        byte *data = pixels.GetData();
-
-        for (uint32 row = 0; row < height / 2; row++) {
-            byte *source = data + (row * stride);
-            byte *destination = data + (((height - 1) - row) * stride);
-
-            std::memcpy(temp_buffer_data, source, stride);
-            std::memcpy(source, destination, stride);
-            std::memcpy(destination, temp_buffer_data, stride);
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLenum GetTextureFormat(TextureFormat format) {
-        switch (format) {
-            case TextureFormat::RGB24: return GL_RGB;
-            case TextureFormat::RGBA32: return GL_RGBA;
-            case TextureFormat::R8: return GL_RED;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLenum GetTextureFormatType(TextureFormat format_type) {
-        switch (format_type) {
-            case TextureFormat::RGB24: return GL_UNSIGNED_BYTE;
-            case TextureFormat::RGBA32: return GL_UNSIGNED_BYTE;
-            case TextureFormat::R8: return GL_UNSIGNED_BYTE;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLenum GetTextureInternalFormat(TextureFormat internal_format) {
-        switch (internal_format) {
-            case TextureFormat::RGB24: return GL_RGB8;
-            case TextureFormat::RGBA32: return GL_RGBA8;
-            case TextureFormat::R8: return GL_R8;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLint GetTextureWrapMode(TextureWrapMode wrap_mode) {
-        switch (wrap_mode) {
-            case TextureWrapMode::Clamp: return GL_CLAMP_TO_EDGE;
-            case TextureWrapMode::Border: return GL_CLAMP_TO_BORDER;
-            case TextureWrapMode::Repeat: return GL_REPEAT;
-            case TextureWrapMode::MirroredRepeat: return GL_MIRRORED_REPEAT;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLint GetTextureMinFilter(TextureFilter filter) {
-        switch (filter) {
-            case TextureFilter::Point: return GL_NEAREST_MIPMAP_NEAREST;
-            case TextureFilter::Bilinear: return GL_LINEAR_MIPMAP_NEAREST;
-            case TextureFilter::Trilinear: return GL_LINEAR_MIPMAP_LINEAR;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return GL_NEAREST_MIPMAP_NEAREST;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLint GetTextureMaxFilter(TextureFilter filter) {
-        switch (filter) {
-            case TextureFilter::Point: return GL_NEAREST;
-            case TextureFilter::Bilinear: return GL_LINEAR;
-            case TextureFilter::Trilinear: return GL_LINEAR;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return GL_NEAREST;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLfloat GetTextureAnisotropicFilter(TextureAnisotropicFilter anisotropic_filter) {
-        switch (anisotropic_filter) {
-            case TextureAnisotropicFilter::None: return 1.0f;
-            case TextureAnisotropicFilter::Times2: return 2.0f;
-            case TextureAnisotropicFilter::Times4: return 4.0f;
-            case TextureAnisotropicFilter::Times8: return 8.0f;
-            case TextureAnisotropicFilter::Times16: return 16.0f;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 1.0f;
-        }
-    }
-    
-    //--------------------------------------------------------------
-    GLenum GetRenderTextureInternalFormat(RenderTextureFormat internal_format) {
-        switch (internal_format) {
-            case RenderTextureFormat::RGBA32: return GL_RGBA8;
-            case RenderTextureFormat::UInt32: return GL_R32UI;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLenum GetGLTopology(MeshTopology mesh_topology) {
-        switch (mesh_topology) {
-            case MeshTopology::Points: return GL_POINTS;
-            case MeshTopology::Lines: return GL_LINES;
-            case MeshTopology::LineStrip: return GL_LINE_STRIP;
-            case MeshTopology::Triangles: return GL_TRIANGLES;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLuint GetGLAttributeIndexForVertextAttributeSize(VertexAttributeKind kind) {
-        switch (kind) {
-            case VertexAttributeKind::Position: return 0;
-            case VertexAttributeKind::Normal: return 1;
-            case VertexAttributeKind::Tangent: return 2;
-            case VertexAttributeKind::Color: return 3;
-            case VertexAttributeKind::Texture0: return 4;
-            case VertexAttributeKind::Texture1: return 5;
-            case VertexAttributeKind::Texture2: return 6;
-            case VertexAttributeKind::Texture3: return 7;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLenum GetGLVertexAttributeType(VertexAttributeType vertex_attribute_type) {
-        switch (vertex_attribute_type) {
-            case VertexAttributeType::Float32: return GL_FLOAT;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLuint GetGLVertexAttributeSizeForVertexAttribute(VertexAttributeType vertex_attribute_type, uint32 dimension) {
-        switch (vertex_attribute_type) {
-            case VertexAttributeType::Float32: return sizeof(float32) * dimension;
-            default: HYP_ASSERT_ENUM_OUT_OF_RANGE; return 0;
-        }
-    }
-
-    //--------------------------------------------------------------
-    GLbitfield GetGLClearFlags(ClearFlags clear_flags) {
-        GLbitfield result = 0;
-        if ((clear_flags & ClearFlags::Color) == ClearFlags::Color) {
-            result |= GL_COLOR_BUFFER_BIT;
-        }
-        if ((clear_flags & ClearFlags::Depth) == ClearFlags::Depth) {
-            result |= GL_DEPTH_BUFFER_BIT;
-        }
-        if ((clear_flags & ClearFlags::Stencil) == ClearFlags::Stencil) {
-            result |= GL_STENCIL_BUFFER_BIT;
-        }
-        return result;
-    }
 
     //--------------------------------------------------------------
     void OpenGLRenderDriver::Initialize() {
@@ -261,7 +81,7 @@ namespace Hyperion::Rendering {
                                 
                                 // We have to make sure that we can clear the depth buffer by enabling the depth mask.
                                 glDepthMask(GL_TRUE);
-                                glClear(GetGLClearFlags(clear_render_target.flags));
+                                glClear(OpenGLUtilities::GetClearFlags(clear_render_target.flags));
                                 break;
                             }
                             case RenderFrameCommandBufferCommandType::SetRenderTarget: {
@@ -711,7 +531,7 @@ namespace Hyperion::Rendering {
         m_stats.vertex_count += sub_mesh.vertex_count;
         m_stats.triangle_count += sub_mesh.index_count;
 
-        GLenum topology = GetGLTopology(sub_mesh.topology);
+        GLenum topology = OpenGLUtilities::GetTopology(sub_mesh.topology);
         glDrawElementsBaseVertex(topology, sub_mesh.index_count, GL_UNSIGNED_INT, index_offset, sub_mesh.vertex_offset);
     }
 
@@ -862,14 +682,14 @@ namespace Hyperion::Rendering {
         TextureFormat format = texture_2d.parameters.format;
         GLsizei width = texture_2d.parameters.width;
         GLsizei height = texture_2d.parameters.height;
-        GLenum internal_format = GetTextureInternalFormat(format);
+        GLenum internal_format = OpenGLUtilities::GetTextureInternalFormat(format);
         GLsizei mipmap_count = Math::Max(texture_2d.mipmap_count, 1);
         glTextureStorage2D(opengl_texture.texture, mipmap_count, internal_format, width, height);
 
-        FlipTextureHorizontally(texture_2d.parameters.width, texture_2d.parameters.height, format, texture_2d.pixels);
-        GLenum format_value = GetTextureFormat(format);
-        GLenum format_type = GetTextureFormatType(format);
-        SetUnpackAlignmentForTextureFormat(format);
+        OpenGLUtilities::FlipTextureHorizontally(texture_2d.parameters.width, texture_2d.parameters.height, format, texture_2d.pixels);
+        GLenum format_value = OpenGLUtilities::GetTextureFormat(format);
+        GLenum format_type = OpenGLUtilities::GetTextureFormatType(format);
+        OpenGLUtilities::SetUnpackAlignmentForTextureFormat(format);
         glTextureSubImage2D(opengl_texture.texture, 0, 0, 0, width, height, format_value, format_type, texture_2d.pixels.GetData());
 
         if (texture_2d.parameters.attributes.use_mipmaps) {
@@ -913,7 +733,7 @@ namespace Hyperion::Rendering {
                 TextureAttributes attributes = attachment.attributes;
                 SetTextureAttributes(opengl_attachment.attachment, attributes);
 
-                GLenum internal_format = GetRenderTextureInternalFormat(attachment.format);
+                GLenum internal_format = OpenGLUtilities::GetRenderTextureInternalFormat(attachment.format);
                 GLsizei mipmap_count = attributes.use_mipmaps ? Math::Max(render_texture.mipmap_count, 1) : 1;
                 glTextureStorage2D(opengl_attachment.attachment, mipmap_count, internal_format, width, height);
 
@@ -1025,16 +845,16 @@ namespace Hyperion::Rendering {
         glVertexArrayElementBuffer(opengl_mesh.vertex_array, opengl_mesh.index_buffer);
 
         for (VertexAttribute vertex_attribute : vertex_format.attributes) {
-            GLuint attribute_index = GetGLAttributeIndexForVertextAttributeSize(vertex_attribute.kind);
+            GLuint attribute_index = OpenGLUtilities::GetAttributeIndexForVertextAttributeSize(vertex_attribute.kind);
             GLuint binding_index = 0;
             GLint size = vertex_attribute.dimension;
-            GLenum type = GetGLVertexAttributeType(vertex_attribute.type);
+            GLenum type = OpenGLUtilities::GetVertexAttributeType(vertex_attribute.type);
 
             glEnableVertexArrayAttrib(opengl_mesh.vertex_array, attribute_index);
             glVertexArrayAttribFormat(opengl_mesh.vertex_array, attribute_index, size, type, false, relative_offset);
             glVertexArrayAttribBinding(opengl_mesh.vertex_array, attribute_index, binding_index);
 
-            relative_offset += GetGLVertexAttributeSizeForVertexAttribute(vertex_attribute.type, vertex_attribute.dimension);
+            relative_offset += OpenGLUtilities::GetVertexAttributeSizeForVertexAttribute(vertex_attribute.type, vertex_attribute.dimension);
         }
 
         m_opengl_meshes.Insert(mesh.id, opengl_mesh);
@@ -1042,16 +862,16 @@ namespace Hyperion::Rendering {
 
     //--------------------------------------------------------------
     void OpenGLRenderDriver::SetTextureAttributes(GLuint texture, TextureAttributes attributes) {
-        GLint wrap_mode = GetTextureWrapMode(attributes.wrap_mode);
+        GLint wrap_mode = OpenGLUtilities::GetTextureWrapMode(attributes.wrap_mode);
         glTextureParameteri(texture, GL_TEXTURE_WRAP_S, wrap_mode);
         glTextureParameteri(texture, GL_TEXTURE_WRAP_T, wrap_mode);
 
-        GLint min_filter = GetTextureMinFilter(attributes.filter);
-        GLint mag_filter = GetTextureMaxFilter(attributes.filter);
+        GLint min_filter = OpenGLUtilities::GetTextureMinFilter(attributes.filter);
+        GLint mag_filter = OpenGLUtilities::GetTextureMaxFilter(attributes.filter);
         glTextureParameteri(texture, GL_TEXTURE_MIN_FILTER, min_filter);
         glTextureParameteri(texture, GL_TEXTURE_MAG_FILTER, mag_filter);
 
-        GLfloat anisotropic_filter_value = GetTextureAnisotropicFilter(attributes.anisotropic_filter);
+        GLfloat anisotropic_filter_value = OpenGLUtilities::GetTextureAnisotropicFilter(attributes.anisotropic_filter);
         glTextureParameterf(texture, GL_TEXTURE_MAX_ANISOTROPY, anisotropic_filter_value);
     }
 
