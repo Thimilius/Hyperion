beb8af1c52ac2e82be9a13e3df5e27b689c80389
Thimilius <mail@thimilius.com>
2022-05-15 17:06:07 +0200

    Add new reflection header

---
+++ hyperion/include/hyperion/core/reflection.hpp
@@ -0,0 +1,26 @@
+﻿//------------------------ Header Guard ------------------------
+#pragma once
+
+//---------------------- Library Includes ----------------------
+#include <rttr/registration.h>
+
+//-------------------- Definition Namespace --------------------
+namespace Hyperion {
+
+  using Type = rttr::type;
+  using Variant = rttr::variant;
+  using Instance = rttr::instance;
+
+  class Reflection final {
+  public:
+    inline static Instance CreateInstanceFromType(void *data, Type type) {
+      Instance i = data;
+      (reinterpret_cast<rttr::detail::data_address_container *>(&i))->m_type = type;
+      return i;
+    }
+  private:
+    Reflection() = delete;
+    ~Reflection() = delete;
+  };
+  
+}
