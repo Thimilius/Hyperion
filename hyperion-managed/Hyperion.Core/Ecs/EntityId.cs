31a2630923696bb151365ea4ed762673bf337e7c
Thimilius <mail@thimilius.com>
2022-06-04 17:57:14 +0200

    Add managed EntityId type

---
+++ hyperion-managed/Hyperion.Core/Ecs/EntityId.cs
@@ -0,0 +1,21 @@
+﻿namespace Hyperion.Ecs {
+  public readonly struct EntityId {
+    public static EntityId Empty => new EntityId(0xFFFFFFFF); 
+    
+    public long Value { get; }
+
+    public int Index => (int)Value;
+    public int Version => (int)(Value >> 32);
+    
+    public EntityId(long value) { Value = value; }
+    
+    public override bool Equals(object obj) => obj is EntityId other && Equals(other); 
+    public override int GetHashCode() => Value.GetHashCode();
+
+    public static implicit operator long(EntityId id) => id.Value;
+    public static implicit operator EntityId(long value) => new(value);
+    
+    public static bool operator ==(EntityId a, EntityId b) => a.Value == b.Value;
+    public static bool operator !=(EntityId a, EntityId b) => a.Value == b.Value;
+  }
+}
